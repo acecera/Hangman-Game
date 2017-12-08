87fe419fd4e3c835f3138a0ead4b19a311a3e98c
Anthony Cera <ace_cera@yahoo.com>
2017-12-08 00:27:06 -0800

    started js

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var pickWord = function() {
+    var words = ['Liar Liar', 'Yes Man', 'Dumb and Dumber', 'The Mask', 
+                  'Ace Ventura', 'Batman Forever'];
+    return
+    words[Math.floor(Math.random() * words.length)];              
+};
+
+var setupAnswerArray
+
+
