2b7750af200427519512723db573afd13944b399
Anthony Cera <ace_cera@yahoo.com>
2017-12-09 14:06:28 -0800

    fixed for loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,16 @@
 var words = ['Liar Liar', 'Yes Man', 'Dumb and Dumber', 'The Mask', 
 'Ace Ventura', 'Batman Forever'];
 var wins = 0;
+var letters = char [] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 var guessesRemaining = 0;
-
+var lettersGuessed = 0;
 var winsElement = document.getElementById("words-guessed-correctly");
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
     var chooseWord = words[Math.floor(Math.random() * words.length)];
     console.log(chooseWord);
-}
+
 
 
 
