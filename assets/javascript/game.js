c16b2e558051a2144cdc53699380877d95a05841
Anthony Cera <ace_cera@yahoo.com>
2017-12-08 23:09:30 -0800

    added char array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,15 @@
-var pickWord = function() {
-    var words = ['Liar Liar', 'Yes Man', 'Dumb and Dumber', 'The Mask', 
-                  'Ace Ventura', 'Batman Forever'];
-    return
-    words[Math.floor(Math.random() * words.length)];              
-};
+var words = ['Liar Liar', 'Yes Man', 'Dumb and Dumber', 'The Mask', 
+'Ace Ventura', 'Batman Forever'];
+var wins = 0;
+var guessesRemaining = 0;
 
-var setupAnswerArray
+var winsElement = document.getElementById("words-guessed-correctly");
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    var chooseWord = words[Math.floor(Math.random() * words.length)];
+    console.log(chooseWord);
+}
+
 
 
