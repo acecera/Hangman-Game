23d88ec6433193dba5bb20478ddb98825a6ff092
Anthony Cera <ace_cera@yahoo.com>
2017-12-10 11:55:51 -0800

    added if else if function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,27 @@
-var words = ['Liar Liar', 'Yes Man', 'Dumb and Dumber', 'The Mask', 
+var words = ['liar liar', 'Yes Man', 'Dumb and Dumber', 'The Mask', 
 'Ace Ventura', 'Batman Forever'];
+var dashWords = [_ _ _ _, ]
 var wins = 0;
-var letters = char [] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 var guessesRemaining = 0;
 var lettersGuessed = 0;
 var winsElement = document.getElementById("words-guessed-correctly");
 
+var chooseWord = words[Math.floor(Math.random() * words.length)];
+console.log(chooseWord);
+var chooseWordArray = chooseWord.split("");
+// for loop     
 document.onkeyup = function (event) {
-    var userGuess = event.key;
-    var chooseWord = words[Math.floor(Math.random() * words.length)];
-    console.log(chooseWord);
+    var userGuess = event.key;   
+    //console.log(userGuess);      
+    for (var i = 0; i < chooseWordArray.length; i++){
+       if(chooseWordArray[i] === userGuess) {
+           console.log(userGuess);
+           dashes[i] = userGuess;
+       }else if(chooseWordArray[i] !== userGuess) {
+           console.log("Wrong!");
+       }
+    }
 
 
 
-
+}
