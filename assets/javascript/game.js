414e618d93ad22bea43c908b23becf8adf7f4e52
Anthony Cera <ace_cera@yahoo.com>
2018-07-24 20:53:21 -0700

    redid all the logic for the hangman game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,149 @@
-var words = ['burton', 'lib-tech', 'capita', 'ride', 
-'arbor', 'gnu'];
-var chooseWord = words[Math.floor(Math.random() * words.length)];
-var brandDiv = document.getElementsByClassName('brand')[0];
-//var expectedDiv = document.getElementsByClassName('expected-brand')[0];
-var dashWords = [('_', '_', '_', '_', '_', '_'), ('_','_','_', '_','_','_','_'), 
-('_','_','_','_','_','_'), ('_','_','_','_'), ('_','_','_','_','_'), ('_','_','_')];
-var wins = 0;
-var guessesRemainingElement = document.getElementsByClassName("guessesRemaining")[6];
-var lettersGuessed = [];
-var winsElement = document.getElementsByClassName("words-guessed-correctly");
-var chooseWordArray = chooseWord.split("");
-// for loop     
-document.onkeyup = function (event) {
-    var userGuess = event.key;   
-}     
-    for (var i = 0; i < chooseWordArray.length; i++) {
-       var span = document.createElement("span")
-       span.textContent = '_';
-       span.style = "margin-left: 5px;";
-       
-    }   
-       if (chooseWordArray.includes(userGuess)) {
-           console.log(userGuess);
-       } else {
-        console.log("Wrong Letter" + userGuess);
-    };
-             
-     var wins = function () {
-        if (dashWords.join("") === chooseWordArray) {
-            wins = wins + 1;
-            endElement.textContent = "You Win!"
-        }    
+var hangmanGame = {
+
+    wordsToPick: {
+        nirvana: {
+            picture: "nirvana.jpg"
+        },
+        weezer: {
+            picture: "weezer.jpg"
+        },
+        metallica: {
+            picture: "metallica.jpg"
+        },
+        blink182: {
+            picture: "blink182.jpg"
+        },
+        u2: {
+            picture: "u2.jpg"
+        },
+        pantera: {
+            picture: "pantera.jpg"
+        },
+        nsync: {
+            picture: "nsync.jpg"
+        },
+        bush: {
+            picture: "bush.jpg"
+        },
+        eminem: {
+            picture: "eminem.jpg"
+        },
+        tupac: {
+            picture: "tupac.jpg"
+        },
+        cardigans: {
+            picture: "theCardigans.jpg"
+        }
+    },
+
+    wordInPlay: null, 
+    lettersInWord: [], 
+    matchedLetters: [], 
+    guessedLetters: [],
+    guessesLeft: 0,
+    totalGuesses: 0,
+    letterChosen: null, 
+    win: 0,
+
+    startGame: function() {
+        var objKeys = Object.keys(this.wordsToPick);
+        this.wordInPlay = objKeys[Math.floor(Math.random() * objKeys.length)];
+        this.lettersInWord = this.wordsInPlay.split("");
+        this.rebuildWordChosen();
+        this.processUpdateTotalGuesses();
+    },
+
+    updatePage: function(letter) {
+        if (this.guessesLeft === 0) {
+            this.restartGame();
+        } else {
+            this.updateGuesses(letter);
+            this.updateMatchedLetters(letter);
+            this.rebuildWordChosen();
+            if (this.updateWins() === true) {
+                this.restartGame();
+            }
+        }
+    },
+
+    updateGuesses: function(letter) {
+        if ((this.letterChosen.indexOf(letter) === -1) && (this.lettersInWord.indexOf(letter) === -1)) {
+            this.letterChosen.push(letter);
+            this.guessesLeft--;
+            document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+            document.querySelector("guesses-left").innerHTML = this.letterChosen.join(', ');
+        }
+    },
+
+    processUpdateTotalGuesses: function() {
+        this.totalGuesses = this.lettersInWord.length + 5;
+        this.guessesLeft = this.totalGuesses;
+        document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+    },
+
+    updateMatchedLetters: function(letter) {
+        for (var i = 0; i < this.lettersInWord.length; i++) {
+            if ((letter === this.lettersInWord[i]) && (this.matchedLetters.indexOf(letter) === -1)) {
+                this.matchedLetters.push(letter);
+            }
+        }
+    },
+
+    rebuildWordChosen: function() {
+        var wordChosen = "";
+        for (var i = 0; i < this.lettersInWord.length; i++) {
+            if (this.matchedLetters.indexOf(this.lettersInWord[i]) !== -1) {
+                wordChosen += this.lettersInWord[i];
+            } else {
+                wordChosen += "&nbsp;_&nbsp;";
+            }
+        }
+        document.querySelector("#current-word").innerHTML = wordChosen;
+    },
+
+    restartGame: function() {
+        document.querySelector("#guesses-letters").innerHTML = "";
+        this.wordInPlay = null; 
+        this.lettersInWord = [];
+        this.matchedLetters = [];
+        this.guessedLetters = [];
+        this.guessesLeft = 0;
+        this.totalGuesses = 0;
+        this.letterChosen = null;
+        this.startGame();
+        this.rebuildWordChosen();
+    },
+
+    updateWins: function() {
+        var win;
+        if (this.matchedLetters.length === 0) {
+            win = false;
+        } else {
+            win = true;
+        }
+
+        for (var i = 0; i < this.lettersInWord.length; i++) {
+            if (this.matchedLetters.indexOf(this.lettersInWord[i]) === -1) {
+                win = false;
+            }
+        }
+
+        if (win) {
+            this.wins = this.wins + 1;
+            document.querySelector("#wins").innerHTML = this.wins;
+            document.querySelector("#bandDiv").innerHTML = 
+             "<img class='band-image' src='images/" + this.wordsToPick[this.wordInPlay].picture + "'>";
+            return true;
+        }
+        return false;
     }
+};
+
+hangmanGame.startGame();
+
+document.onkeyup = function(event) {
+    hangmanGame.letterChosen = String.fromCharCode(event.keyCode).toLowerCase();
+    hangmanGame.updatePage(hangmanGame.letterChosen);
+};
 
 
